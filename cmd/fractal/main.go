bfe94ea91f6fbf8b9e7e07e543b714fed8e37f00
lilythecat859 <lilythecat859@gmail.com>
2025-10-08 03:00:46 -0400

    Update main.go

--- cmd/fractal/main.go
+++ cmd/fractal/main.go
@@ -1,12 +1,6 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
 	"github.com/lilythecat859/fractal-rpc/internal/config"
 	"github.com/lilythecat859/fractal-rpc/internal/server"
 	"go.uber.org/zap"
@@ -14,27 +8,9 @@ import (
 
 func main() {
 	cfg := config.MustLoad()
-
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
-
-	srv := server.New(cfg, logger)
-
-	go func() {
-		if err := srv.Start(); err != nil {
-			logger.Fatal("server failed", zap.Error(err))
-		}
-	}()
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-	<-quit
-
-	logger.Info("shutting down gracefully")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := srv.Stop(ctx); err != nil {
-		logger.Fatal("forced shutdown", zap.Error(err))
+	if err := server.Run(cfg, logger); err != nil {
+		logger.Fatal("server exited", zap.Error(err))
 	}
-	logger.Info("server stopped")
 }
