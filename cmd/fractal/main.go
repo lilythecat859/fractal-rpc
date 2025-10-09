5db00f3527992778f64b643ad7ef0ed27e3aa1c0
lilythecat859 <lilythecat859@gmail.com>
2025-10-09 03:30:54 -0400

    Update main.go

--- cmd/fractal/main.go
+++ cmd/fractal/main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
-	"github.com/lilythecat859/fractal-rpc/internal/config"
-	"github.com/lilythecat859/fractal-rpc/internal/server"
+	"context"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
 	"go.uber.org/zap"
 )
 
 func main() {
-	cfg := config.MustLoad()
-	logger, _ := zap.NewProduction()
+	logger, _ := zap.NewDevelopment()
 	defer logger.Sync()
-	if err := server.Run(cfg, logger); err != nil {
-		logger.Fatal("server exited", zap.Error(err))
-	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"status":"ok"}`))
+	}).Methods("GET")
+
+	srv := &http.Server{Addr: ":8080", Handler: r}
+	ln, _ := net.Listen("tcp", srv.Addr)
+	logger.Info("listening", zap.String("addr", ln.Addr().String()))
+
+	go srv.Serve(ln)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	srv.Shutdown(ctx)
 }
