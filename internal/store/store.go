c61ee91c25812f529451e8f3dc48231dc784de81
lilythecat859 <lilythecat859@gmail.com>
2025-10-10 02:07:35 -0400

    Update store.go

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,5 +1,14 @@
+// internal/store/store.go
 package store
 
+import (
+	"context"
+	"github.com/gagliardetto/solana-go"
+)
+
 type Store interface {
+	GetSlot(ctx context.Context) (uint64, error)
+	GetBlock(ctx context.Context, slot uint64) (*solana.Block, error)
+	GetBlockTime(ctx context.Context, slot uint64) (int64, error)
 	Close() error
 }
