e46c3a8e7a74febd80d035978f5436721099f28c
lilythecat859 <lilythecat859@gmail.com>
2025-10-11 03:08:40 -0400

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,63 +1,36 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+)
 
 type Config struct {
-	HTTPPort   int            `mapstructure:"http_port"`
-	RPCPath    string         `mapstructure:"rpc_path"`
-	JWTSecret  string         `mapstructure:"jwt_secret"`
-	ClickHouse ClickHouseConf `mapstructure:"clickhouse"`
-	S3         S3Conf         `mapstructure:"s3"`
-	Fractal    FractalConf    `mapstructure:"fractal"`
-	srv        Server
-}
-
-type ClickHouseConf struct {
-	Addr     string            `mapstructure:"addr"`
-	Database string            `mapstructure:"database"`
-	Auth     struct {
-		Username string `mapstructure:"username"`
-		Password string `mapstructure:"password"`
-	} `mapstructure:"auth"`
-	Codec map[string]string `mapstructure:"codec"`
-}
-
-type S3Conf struct {
-	Bucket    string `mapstructure:"bucket"`
-	Region    string `mapstructure:"region"`
-	AccessKey string `mapstructure:"access_key"`
-	SecretKey string `mapstructure:"secret_key"`
-	Endpoint  string `mapstructure:"endpoint"`
-}
-
-type FractalConf struct {
-	ShardBits       uint8   `mapstructure:"shard_bits"`
-	CacheFraction   float64 `mapstructure:"cache_fraction"`
-	ParquetPageSize int     `mapstructure:"parquet_page_size"`
-}
-
-type Server struct {
-	Addr string `mapstructure:"addr"`
+	HTTPPort   int    `mapstructure:"http_port"`
+	JWTSecret  string `mapstructure:"jwt_secret"`
+	ClickHouse struct {
+		Addr     string `mapstructure:"addr"`
+		Database string `mapstructure:"database"`
+		Auth     struct {
+			Username string `mapstructure:"username"`
+			Password string `mapstructure:"password"`
+		} `mapstructure:"auth"`
+	} `mapstructure:"clickhouse"`
+	S3 struct {
+		Bucket    string `mapstructure:"bucket"`
+		Region    string `mapstructure:"region"`
+		AccessKey string `mapstructure:"access_key"`
+		SecretKey string `mapstructure:"secret_key"`
+		Endpoint  string `mapstructure:"endpoint"`
+	} `mapstructure:"s3"`
 }
 
 func MustLoad() *Config {
-	viper.SetConfigFile("fractal.toml")
-	if err := viper.ReadInConfig(); err != nil {
-		panic(err)
-	}
-	viper.SetDefault("http_port", 8899)
-	viper.SetDefault("rpc_path", "/")
-
+	viper.SetConfigName("fractal")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("/etc")
+	viper.AddConfigPath(".")
+	_ = viper.ReadInConfig()
 	var c Config
-	if err := viper.Unmarshal(&c); err != nil {
-		panic(err)
-	}
+	_ = viper.Unmarshal(&c)
 	return &c
-}
-
-func (c *Config) Server() *Server {
-	if c.srv.Addr == "" {
-		c.srv.Addr = ":8080"
-	}
-	return &c.srv
 }
